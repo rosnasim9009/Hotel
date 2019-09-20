db5f6b9a1afcaf77dff400cdc4a9f6f9796d9d79
Rosna Simon <rosnasimon22@gmail.com>
2019-09-20 19:52:58 +0530

    case study 4

--- Hotel Management/src/hotelmng/Hotel.java
+++ Hotel Management/src/hotelmng/Hotel.java
@@ -19,14 +19,15 @@ public class Hotel {
 		this.proofId=proofId;
 	}
 	public void register(String name,String address,String contactNumber,String email,String proofType,String proofId){
-		System.out.println("\nYour Details are....\n");
-		System.out.println(this.name);
-		System.out.println(this.address);
-		System.out.println(this.contactNumber);
-		System.out.println(this.email);
-		System.out.println(this.proofType);
-		System.out.println(this.proofId);
-		System.out.println("Thanks for Registering!!!!\tYour ID is "+ ++registerId);
+		System.out.println("\nYour Details are....");
+		System.out.println("Name			:"+this.name);
+		System.out.println("Address			:"+this.address);
+		System.out.println("Phone Number		:"+this.contactNumber);
+		System.out.println("Email			:"+this.email);
+		System.out.println("Proof Type		:"+this.proofType);
+		System.out.println("Proof ID		:"+this.proofId);
+		System.out.println("\nThanks for Registering!!!!\tYour ID is "+ ++registerId);
+		//System.out.println("--------------------------------------------------------------------");
 	}
 
 }
--- Hotel Management/src/hotelmng/Main.java
+++ Hotel Management/src/hotelmng/Main.java
@@ -8,51 +8,97 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
+		Room r=new Room();
+		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String ans="yes";
-		while(true) {
-			
-		System.out.println("\nEnter your Details....\n");
+		do{
+		System.out.println("Welcome to HOTEL TAJ!!!!");	
+		System.out.println("````````````````````````````");
+		System.out.println("Registration Form");
+		System.out.print("\nEnter your Name		: ");
 		String name=br.readLine();
+		System.out.print("Enter your Address	: ");
 		String address=br.readLine();
+		System.out.print("Enter your Phone Number	: ");
 		String contactNumber=br.readLine();
+		System.out.print("Enter your Email	: ");
 		String email=br.readLine();
+		System.out.print("Enter your Proof Type	: ");
 		String proofType=br.readLine();
+		System.out.print("Enter your Proof ID	: ");
 		String proofId=br.readLine();
 		
 		Hotel customer=new Hotel(name,address,contactNumber,email,proofType,proofId);
 		customer.register(name,address,contactNumber,email,proofType,proofId);
-		System.out.println("\nDo you want to continue? yes/no: ");
+		System.out.println("Do you want to continue? (yes/no) : ");
 		ans=br.readLine();
-		if(ans.equals("no")) {
-			break;
-		}
-	}
+	}while(ans.equalsIgnoreCase("yes"));
 	
-	while(true) {	
+		do{
+	System.out.println("\n\t````````````````````");
+	System.out.println("\n\tMENU");
+	System.out.println("\t1. Book");
+	System.out.println("\t2. Check Status");
+	System.out.println("\t3. Exit");
+	System.out.println("\t````````````````````");
+	System.out.print("Enter Your Choice		: ");
+	int ch=Integer.parseInt(br.readLine());
+	switch(ch) {
+	case 1:
+		
+	
+	do {	
 	System.out.println("\nBooking: ");
+	System.out.println("```````````````````");
 	System.out.println("Please choose the services required..");
-	System.out.println("Ac/Non-Ac(Ac/nAc)");
+	System.out.print("Ac/Non-Ac(Ac/nAc)		:");
 	String ac=br.readLine();
-	System.out.println("Cot(Single/Double)");
+	System.out.print("Cot(Single/Double)		:");
 	String cot=br.readLine();
-	System.out.println("With Cable connection/Without Cable Connection(C/nC)");
+	System.out.println("\nWith Cable connection/");
+	System.out.print("Without Cable Connection(C/nC)	:");
 	String cable=br.readLine();
-	System.out.println("Wi-Fi needed or not(W/nW)");
+	System.out.print("Wi-Fi needed or not(W/nW)	:");
 	String wifi=br.readLine();
-	System.out.println("Laundry service needed or not(L/nL)");
+	System.out.print("Laundry needed or not(L/nL)	:");
 	String lan=br.readLine();
-	System.out.println("Date: ");
+	System.out.print("Date				: ");
 	String date=br.readLine();
 	
-	Room r=new Room(ac,cot,cable,wifi,lan,date);
+	r.ac=ac;
+	r.cot=cot;
+	r.cable=cable;
+	r.wifi=wifi;
+	r.laundry=lan;
+	r.date=date;
+	r.charge=0;
 	r.book(ac, cot, cable, wifi, lan,date);
-	System.out.println("\nDo you want to proceed? (yes/no): ");
+	System.out.print("\nDo you want to proceed? (yes/no)	: ");
 	ans=br.readLine();
-	if(ans.equals("yes")) {
+	if(ans.equalsIgnoreCase("yes")) {
+		
 		System.out.println("\nThank you for booking. Your room number is "+r.roomId);
 		break;
 	}
-	}
+	}while(true);
+	
+	break;
+	
+	case 2:
+		System.out.println("`````````````````````````");
+		System.out.print("Enter room number	: ");
+		int roomId=Integer.parseInt(br.readLine());
+		r.CheckStatus(roomId);
+		break;
+		
+	case 3:
+		System.out.println("!!!!!THANK YOU!!!!!");
+		System.exit(0);
+	default:
+		System.out.println("Invalid Input!!!");
+		
    }
+	}while(true);
+	}
 }
